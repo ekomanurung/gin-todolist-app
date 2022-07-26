65b0f8eb04d96da12968df2d910f02c444bfcf83
Eko Januardy Manurung <eko.manurung@grabtaxi.com>
2022-07-26 15:19:20 +0700

    Initialize Database using mysql

--- main.go
+++ main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"gin-todolist/todo/handler"
 	"gin-todolist/todo/repository"
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"time"
 )
 
 func main() {
 	r := gin.New()
 
-	todoRepository := repository.NewMysqlTodoRepository()
+	//initialize mysql configuration
+	//TODO remove parseTime attribute, since we should define the date time in db in a right way
+	db, sqlErr := sql.Open("mysql", "root:root@/todolist?parseTime=true")
+	if sqlErr != nil {
+		panic(fmt.Sprintf("Panic when initialize mysql connection caused by: %+v\n", sqlErr.Error()))
+	}
+
+	//defer db.Close()
+
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+	db.SetConnMaxLifetime(time.Minute * 20)
+
+	todoRepository := repository.NewMysqlTodoRepository(db)
 	handler.NewTodoHandler(r, todoRepository)
 
-	r.Run()
+	err := r.Run()
+	if err != nil {
+		panic(fmt.Sprintf("Panic when starting the web server caused by: %v\n", err.Error()))
+	}
 }
--- todo/repository/TodoMysqlRepository.go
+++ todo/repository/TodoMysqlRepository.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"database/sql"
 	"errors"
 	"fmt"
 	"gin-todolist/model"
@@ -9,55 +10,117 @@ import (
 )
 
 type TodoMysqlRepository struct {
+	db *sql.DB
 }
 
-func NewMysqlTodoRepository() todo.Repository {
-	return &TodoMysqlRepository{}
+func NewMysqlTodoRepository(sql *sql.DB) todo.Repository {
+	return &TodoMysqlRepository{
+		db: sql,
+	}
 }
 
-var todos = []model.Todo{
-	{ID: 1, Title: "Cuci piring", Author: "Eko", CreatedAt: time.Now()},
-	{ID: 2, Title: "Masak nasi", Author: "Eko", CreatedAt: time.Now()},
-}
-
-func (r *TodoMysqlRepository) Save(todo model.Todo) (model.Todo, error) {
+func (r *TodoMysqlRepository) Save(todo *model.Todo) (*model.Todo, error) {
 	todo.CreatedAt = time.Now()
 
-	todos = append(todos, todo)
+	stmt, err := r.db.Prepare("INSERT INTO todo(title, author, created_at) VALUES(? , ? , ?)")
+	if err != nil {
+		fmt.Printf("Error when prepare db statement caused by: %+v\n", err.Error())
+		return nil, err
+	}
+	defer stmt.Close()
 
+	res, err := stmt.Exec(todo.Title, todo.Author, todo.CreatedAt)
+	if err != nil {
+		fmt.Printf("Error when save todo %+v. caused by: %+v\n", todo, err.Error())
+		return nil, err
+	}
+
+	lastInsertedId, err := res.LastInsertId()
+	if err != nil {
+		fmt.Printf("Error when get last inserted id caused by: %+v\n", err.Error())
+	}
+
+	todo.ID = int(lastInsertedId)
+	fmt.Printf("Success insert todo with id : %d\n", lastInsertedId)
 	return todo, nil
 }
 
 func (r *TodoMysqlRepository) GetOne(id int) (*model.Todo, error) {
-	idx := findItemFromDB(id)
+	stmt, err := r.db.Prepare("SELECT * FROM todo where id = ?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
 
-	if idx == -1 {
-		return nil, errors.New(fmt.Sprintf("item with id %d not found", id))
+	todo := &model.Todo{}
+
+	err = stmt.QueryRow(id).Scan(&todo.ID, &todo.Title, &todo.Author, &todo.CreatedAt)
+	if err != nil {
+		fmt.Printf("Error when execute Get By id: %d Query to database caused by: %v\n", id, err.Error())
+		return nil, err
 	}
 
-	return &todos[idx], nil
+	return todo, nil
 }
 
 func (r *TodoMysqlRepository) Delete(id int) (bool, error) {
-	idx := findItemFromDB(id)
-
-	if idx == -1 {
-		return false, errors.New(fmt.Sprintf("item with id %d not found", id))
+	trx, err := r.db.Begin()
+	if err != nil {
+		fmt.Sprintf("Error when create transaction caused by: %+v\n", err)
+		return false, err
 	}
 
-	todos = append(todos[:idx], todos[idx+1:]...)
-	return true, errors.New(fmt.Sprintf("success delete item with id %d", id))
+	stmt, err := trx.Prepare("DELETE from todo where id = (?)")
+	if err != nil {
+		fmt.Sprintf("Error when Prepare Delete Query Statement caused by: %+v\n", err)
+		return false, err
+	}
+
+	result, err := stmt.Exec(id)
+	if err != nil {
+		fmt.Sprintf("Error when Execute Delete Query caused by: %+v\n", err)
+		return false, err
+	}
+
+	err = trx.Commit()
+	if err != nil {
+		fmt.Sprintf("Error when Commit Delete Query caused by: %+v\n", err)
+		return false, err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		fmt.Sprintf("Error when Fetch Delete Result caused by: %+v\n", err)
+		return false, err
+	}
+
+	if rowsAffected > 0 {
+		return true, errors.New(fmt.Sprintf("Success delete todo item with id: %d", id))
+	}
+
+	return false, errors.New(fmt.Sprintf("Failed to delete todo with id: %d, not found", id))
 }
 
-func (r *TodoMysqlRepository) GetAll() []model.Todo {
-	return todos
-}
+func (r *TodoMysqlRepository) GetAll() []*model.Todo {
+	rows, err := r.db.Query("SELECT * FROM todo")
+	if err != nil {
+		fmt.Printf("Error when execute Get All Query to database caused by: %v\n", err.Error())
+		return nil
+	}
+	defer rows.Close()
 
-func findItemFromDB(id int) int {
-	for idx, t := range todos {
-		if t.ID == id {
-			return idx
+	var todos = make([]*model.Todo, 0)
+
+	for rows.Next() {
+		todo := &model.Todo{}
+
+		err := rows.Scan(&todo.ID, &todo.Title, &todo.Author, &todo.CreatedAt)
+		if err != nil {
+			fmt.Printf("Error parsing data todo caused by: %+v\n", err)
 		}
+
+		todos = append(todos, todo)
 	}
-	return -1
+
+	return todos
 }
