fa42228cdf33763de9e9ef92980de7125fb80006
Eko Januardy Manurung <eko.manurung@grabtaxi.com>
2022-08-01 19:37:54 +0700

    Add Struct validation

--- model/todo.go
+++ model/todo.go
@@ -4,7 +4,7 @@ import "time"
 
 type Todo struct {
 	ID        int       `db:"id" json:"id"`
-	Title     string    `db:"title" json:"title"`
-	Author    string    `db:"author" json:"author"`
+	Title     string    `db:"title" json:"title" binding:"required"`
+	Author    string    `db:"author" json:"author" binding:"required"`
 	CreatedAt time.Time `db:"created_at" json:"created_at"`
 }
