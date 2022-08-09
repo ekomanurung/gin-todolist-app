45a95411c3998e4009ba8afb9de7f5cac4e8801a
Eko Januardy Manurung <eko.manurung@grabtaxi.com>
2022-08-09 15:52:04 +0700

    add log

--- main.go
+++ main.go
@@ -29,6 +29,8 @@ func main() {
 	configuration.ConfigureLogLevel()
 	configuration.ConfigureDependencies(r)
 
+	fmt.Printf("COBA LAGI")
+
 	err := r.Run(":8080")
 	if err != nil {
 		panic(fmt.Sprintf("Panic when starting the web server caused by: %v\n", err.Error()))
